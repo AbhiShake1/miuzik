19f4e2d9e250575282e13f4eaab5f0e960b08cc3
abhi <abhishake1>
2022-06-29 23:44:27 +0545

    initialize storage boxes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:miuzik/app/routes/app_pages.dart';
 
-import 'app/routes/app_pages.dart';
+void main() async {
+  //init on storage also ensures widget flutter bindings are initialized
+  final storageBoxes = ['GetStorage', 'settings', 'downloads', 'cache'];
+  for (final box in storageBoxes) {
+    await GetStorage.init(box);
+  }
 
-void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: 'Miuzik',
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
